77e384c707cb1e77a6b67d4dc122a9ca0a77efdb
Thomas personal <thomaspersonal@Thomass-MacBook-Air.local>
2024-11-20 19:30:31 -0300

    commit 2

--- routes/reservasRoutes.js
+++ routes/reservasRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const reservasController = require('../controllers/reservasController'); // Importamos controladores.
+const reservasController = require('../controllers/reservasControllers'); // Importamos controladores.
 
 // Endpoints CRUD
-router.post('/', reservasController.crearReserva); // Crea nueva reserva.
-router.get('/', reservasController.obtenerReservas); // Obtener TODAS las reservas.
-router.get('/:id', reservasController.obtenerReservaPorId); // Obtener reserva por ID.
-router.put('/:id', reservasController.actualizarReserva); // Actualizar reserva (ID).
-router.delete('/:id', reservasController.eliminarReserva); // Eliminar reserva (ID).
+router.post('/', reservasControllers.crearReserva); // Crea nueva reserva.
+router.get('/', reservasControllers.obtenerReservas); // Obtener TODAS las reservas.
+router.get('/:id', reservasControllers.obtenerReservaPorId); // Obtener reserva por ID.
+router.put('/:id', reservasControllers.actualizarReserva); // Actualizar reserva (ID).
+router.delete('/:id', reservasControllers.eliminarReserva); // Eliminar reserva (ID).
 
-router.get('/', reservasController.filtrarReservas); // Filtros
+router.get('/', reservasControllers.filtrarReservas); // Filtros
 
 module.exports = router; // Exportamos los las rutas para ocuparlas en toda la app.
